213026fd2ee21a37103d6ed739331a3403ae7d32
neil.herbet.agtuca <neil.herbet.agtuca@VITA-9467ZVZ-LX.dir.svc.accenture.com>
2021-07-31 16:58:02 +0800

    005 added another console method to Test004

--- eBanking/src/com/eBanking/util/Test004.java
+++ eBanking/src/com/eBanking/util/Test004.java
@@ -3,6 +3,9 @@ package com.eBanking.util;
 public class Test004 {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Hellow world");
+		
 		System.out.println("Hellow world");
 	}
 	
