85e372e5abfec7bbe3b290208566394ceb042d92
neil.herbet.agtuca <neil.herbet.agtuca@VITA-9467ZVZ-LX.dir.svc.accenture.com>
2021-07-31 16:53:39 +0800

    002 added 4 console method for all the class

--- eBanking/src/com/eBanking/util/Test001.java
+++ eBanking/src/com/eBanking/util/Test001.java
@@ -2,4 +2,8 @@ package com.eBanking.util;
 
 public class Test001 {
 
+	public static void main(String[] args) {
+		System.out.println("Hellow world");
+	}
+
 }
--- eBanking/src/com/eBanking/util/Test002.java
+++ eBanking/src/com/eBanking/util/Test002.java
@@ -2,4 +2,8 @@ package com.eBanking.util;
 
 public class Test002 {
 
+	public static void main(String[] args) {
+		System.out.println("Hellow world");
+	}
+
 }
